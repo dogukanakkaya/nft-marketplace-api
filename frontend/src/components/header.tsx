55baf99737409b895107ff4cfb205cecf7f06aa2
dogukanakkaya <doguakkaya27@hotmail.com>
2023-03-12 02:54:53 +0300

    feat: fetch nfts

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,18 +4,22 @@ export function Header() {
     const { isMetamaskLoading, isMetamaskConnected, accounts, connectToMetamask } = useMetamask();
 
     return (
-        <div className="h-20 bg-gray-200 p-4 flex items-center justify-end">
-            {
-                isMetamaskLoading ? (
-                    <span className="bg-gray-600 shadow-gray-500 px-4 py-2 rounded text-white shadow">Loading...</span>
-                ) : (
-                    isMetamaskConnected ? <span className="bg-gray-600 shadow-gray-500 px-4 py-2 rounded text-white shadow">Wallet Connected {accounts[0].address}</span> : (
-                        <button onClick={connectToMetamask} className="bg-green-500 shadow-green-400 hover:bg-green-600 px-4 py-2 rounded text-white shadow transition">
-                            Connect Wallet
-                        </button>
-                    )
-                )
-            }
+        <div className="bg-gray-200">
+            <div className="container mx-auto">
+                <div className="h-20 flex items-center justify-end">
+                    {
+                        isMetamaskLoading ? (
+                            <span className="bg-gray-600 shadow-gray-500 px-4 py-2 rounded text-white shadow">Loading...</span>
+                        ) : (
+                            isMetamaskConnected ? <span className="bg-gray-600 shadow-gray-500 px-4 py-2 rounded text-white shadow">Wallet Connected {accounts[0].address}</span> : (
+                                <button onClick={connectToMetamask} className="bg-green-500 shadow-green-400 hover:bg-green-600 px-4 py-2 rounded text-white shadow transition">
+                                    Connect Wallet
+                                </button>
+                            )
+                        )
+                    }
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/pages/nfts/index.tsx
+++ frontend/src/pages/nfts/index.tsx
@@ -1,7 +1,48 @@
+import { API_URL } from "@/config";
+import { useEffect, useState } from "preact/hooks";
+import check from '@/assets/check-lg.svg';
+
 export function NFTs() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        !async function () {
+            const response = await fetch(`${API_URL}/nfts`, { signal: abortController.signal });
+            const result = await response.json();
+            setData(result);
+        }();
+
+        return () => abortController.abort();
+    }, []);
+
     return (
-        <div>
-            NFTs
+        <div className="container mx-auto mt-8">
+            {
+                data.length ? (
+                    <div className="grid grid-cols-4 gap-8">
+                        {
+                            data.map(item => (
+                                <div className="relative">
+                                    <div>
+                                        <img src={item.image} alt="" />
+                                    </div>
+                                    <div>
+                                        <h2 className="font-semibold text-lg">{item.name}</h2>
+                                        <p className="text-sm">{item.description}</p>
+                                    </div>
+                                    <button className="absolute w-24 right-2 top-2 text-white px-4 py-1 rounded bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-400 hover:to-blue-400 transition">
+                                        Mint
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-check-all inline-block" viewBox="0 0 16 16">
+                                            <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            ))
+                        }
+                    </div>
+                ) : <>Loading todo...</>
+            }
         </div>
     );
-}
+};
