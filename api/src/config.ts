989b1f69170198ea5ff699f21fe26c05c1857d43
dogukanakkaya <doguakkaya27@hotmail.com>
2023-03-13 00:59:55 +0300

    feat: add bucket and role definition to template

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,4 +1,4 @@
 export const REGION = 'us-east-1' as const;
-export const BUCKET = 'lazy-camp' as const;
+export const BUCKET = 'spacer-bucket' as const;
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000' as const;
 export const ALLOW_ORIGIN = process.env.ALLOW_ORIGIN as string;
