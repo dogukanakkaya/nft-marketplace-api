e3d99b98640d2a4e8892cb80293dcaeae161bb5a
dogukanakkaya <doguakkaya27@hotmail.com>
2023-03-14 02:06:06 +0300

    chore: naming practice

--- src/aws-services/secret-manager.ts
+++ src/aws-services/secret-manager.ts
@@ -1,7 +1,7 @@
 import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 
-const smclient = new SecretsManagerClient({});
+const smClient = new SecretsManagerClient({});
 export const getSecret = async (secretId: string) => {
-    const { SecretString } = await smclient.send(new GetSecretValueCommand({ SecretId: secretId }));
+    const { SecretString } = await smClient.send(new GetSecretValueCommand({ SecretId: secretId }));
     return JSON.parse(SecretString);
 }
